f9790eac15b3b337166499fc92f4a15c2728ba07
Admin <Admin@ADMIN-PC>
2024-01-16 01:16:39 +0100

    bisschen mit werten rumspielen

--- dqn_example_no_moving_objects.py
+++ dqn_example_no_moving_objects.py
@@ -10,11 +10,14 @@ import numpy as np
 print("GPU support: ", torch.cuda.is_available())
 
 # Define the environment
-n_rows = 20
-n_cols = 20
+n_rows = 10
+n_cols = 10
 # n_states = n_rows * n_cols
 n_actions = 4 
 actions = ['Up', 'Down', 'Left', 'Right']
+
+goal_field = (n_rows-1, n_cols-1)
+goal_reward = 100
 
 # Define the action-to-index dictionary
 index_to_action = {0: 'Up', 1: 'Down', 2:'Left', 3: 'Right'}
@@ -22,8 +25,8 @@ index_to_action = {0: 'Up', 1: 'Down', 2:'Left', 3: 'Right'}
 class DQN(nn.Module):
     def __init__(self, input_size, output_size):
         super(DQN, self).__init__()
-        self.fc1 = nn.Linear(input_size, 512)
-        self.fc2 = nn.Linear(512, output_size)
+        self.fc1 = nn.Linear(input_size, 128)
+        self.fc2 = nn.Linear(128, output_size)
 
     def forward(self, x):
         x = F.relu(self.fc1(x.view(-1, self.fc1.in_features)))
@@ -31,36 +34,29 @@ class DQN(nn.Module):
 
 # Define rewards and penalties
 rewards = np.zeros((n_rows, n_cols))
-rewards[19][19] = 10
-rewards[10][10] = -1
-rewards[5][5]   = 5
-rewards[15][15] = 5
-rewards[1][0]   = -1
-rewards[2][0]   = -1
-rewards[3][0]   = -1
-rewards[4][0]   = -1
-rewards[5][0]   = -1
-rewards[6][0]   = -1
-rewards[7][0]   = -1
-rewards[5][5]   = -1
+rewards[goal_field[0]][goal_field[1]] = goal_reward
+rewards[5][5]   = 9
+rewards[6][6]   = 9
 
-# rewards = np.zeros(n_states)
-# rewards[2] = -1
-# rewards[4] = -1
-# rewards[8] = 1  # Goal state
+# fields_travelled = []
+def get_reward_for_field(row_idx, col_idx):
+    if rewards[row_idx][col_idx] == 0:
+        return -1
+    else:
+        return rewards[row_idx][col_idx]
 
 # DQN parameters
-gamma = 0.9          # Discount factor
-epsilon = 1.0        # Initial exploration rate
-epsilon_decay = 0.97
+gamma = 0.6          # Discount factor
+epsilon = 1.0      # Initial exploration rate
+epsilon_decay = 0.9997
 min_epsilon = 0.1
 alpha = 0.20
 batch_size = 32
-target_update = 10
-max_episodes = 1000
+target_update = 100
+max_episodes = 4000
 
 # Initialize replay memory
-replay_memory_size = 10000
+replay_memory_size = 4000
 replay_memory = []
 
 # Define named tuple for experience replay
@@ -88,25 +84,61 @@ def calculate_next_state(current_state, action):
         row = max(0, row)
     elif action == 1:  # Move Down
         row = row + 1
-        row = min(row, 19)
+        row = min(row, n_rows-1)
     elif action == 2:  # Move Left
         col = col - 1
         col = max(0,col)
     elif action == 3:  # Move Right
         col = col + 1
-        col = min(col, 19)
+        col = min(col, n_cols-1)
 
     return row, col 
+
+# # Function to select epsilon-greedy action
+# def epsilon_greedy_action(state, epsilon):
+#     if random.uniform(0, 1) < epsilon:
+#         while True:
+#             action = random.randint(0, n_actions - 1)
+#             next_row, next_col = calculate_next_state(state, action)
+#             if not (next_row < 0 or next_row >= n_rows or next_col < 0 or next_col >= n_cols):
+#                 break
+#     else:
+#         state_tensor = torch.tensor([state], dtype=torch.float32)  # Wrap the state in a tensor
+#         q_values = dqn(state_tensor)
+#         max_action = q_values.argmax().item()
+#         # print("q_values.shape = ", q_values.shape)
+#         # print("q_values", q_values)
+#         # print(max_action)
+#         assert(max_action >= 0 and max_action <= 3)
+
+#         next_row, next_col = calculate_next_state(state, max_action)
+#         if next_row < 0 or next_row >= n_rows or next_col < 0 or next_col >= n_cols:
+#             while True:
+#                 action = random.randint(0, n_actions - 1)
+#                 next_row, next_col = calculate_next_state(state, action)
+#                 if not (next_row < 0 or next_row >= n_rows or next_col < 0 or next_col >= n_cols):
+#                     break
+#         else:
+#             action = max_action
+#     return action
+
+def is_allowed_action(next_row, next_col):
+    if next_row >= 0 and next_row < n_rows and next_col >= 0 and next_col < n_cols:
+        return True
+    else:
+        return False
 
 # Function to select epsilon-greedy action
 def epsilon_greedy_action(state, epsilon):
     if random.uniform(0, 1) < epsilon:
+        # Take random action
         while True:
             action = random.randint(0, n_actions - 1)
             next_row, next_col = calculate_next_state(state, action)
-            if not (next_row < 0 or next_row >= n_rows or next_col < 0 or next_col >= n_cols):
+            if is_allowed_action(next_row, next_col):
                 break
     else:
+        # Take action proposed by DQN
         state_tensor = torch.tensor([state], dtype=torch.float32)  # Wrap the state in a tensor
         q_values = dqn(state_tensor)
         max_action = q_values.argmax().item()
@@ -116,41 +148,56 @@ def epsilon_greedy_action(state, epsilon):
         assert(max_action >= 0 and max_action <= 3)
 
         next_row, next_col = calculate_next_state(state, max_action)
-        if next_row < 0 or next_row >= n_rows or next_col < 0 or next_col >= n_cols:
+
+        # If action is not allowed, anyways take allowed random action
+        if not is_allowed_action(next_row, next_col):
             while True:
                 action = random.randint(0, n_actions - 1)
                 next_row, next_col = calculate_next_state(state, action)
-                if not (next_row < 0 or next_row >= n_rows or next_col < 0 or next_col >= n_cols):
+                if is_allowed_action(next_row, next_col):
                     break
         else:
             action = max_action
     return action
 
+goal_reached_counter = 0
 def is_goal_reached(reward):
-    if reward == 10:
+    global goal_reached_counter
+    if reward == goal_reward:
+        goal_reached_counter += 1
+        # print("goal reached!!")
         return True
     else:
         return False
 
+action_counter = [0,0,0,0]
+
 # DQN training loop
-for episode in range(max_episodes):
+for episode_idx in range(max_episodes):
     row = 0
     col = 0
     total_reward = 0
     done = False
     step_count = 0
 
+    if (episode_idx % 1000) == 0:
+        print("Number of trained episodes: ", episode_idx)
+        print("Current epsilon =", epsilon)
+        print("Goal reached:", goal_reached_counter, "times")
+
     while not done:
         state_idx = row * n_cols + col
         # print("state_idx = ", state_idx)
         action = epsilon_greedy_action(state_idx, epsilon)
+        action_counter[action] += 1
         #print(state_idx, index_to_action[action])
         
         # Simulate environment (transition to next state and get reward)
         next_row, next_col = calculate_next_state(state_idx, action)
         next_state_idx = next_row * n_cols + next_col
         
-        reward = rewards[next_row][next_col]
+        # reward = rewards[next_row][next_col]
+        reward = get_reward_for_field(next_row, next_col)
         done = is_goal_reached(reward)                    # Check if goal reached
         total_reward += reward
         
@@ -198,15 +245,17 @@ for episode in range(max_episodes):
         col = next_col
         step_count += 1
 
-        if step_count > 20:  # Terminate the episode if steps exceed 20
+        if step_count > 40:  # Terminate the episode if steps exceed 20
             done = True
 
         # Decay epsilon
         epsilon = max(min_epsilon, epsilon * epsilon_decay)
 
-    if episode % target_update == 0:
+    if episode_idx % target_update == 0:
         target_dqn.load_state_dict(dqn.state_dict())  # Update target network
 
+print("                                 ", actions)
+print("Actions performed during training", action_counter)
 
 # After training, generate actions for an episode
 generated_actions = []
@@ -220,13 +269,13 @@ while not done:
     next_row, next_col = calculate_next_state(state, action)
     action = torch.topk(q_values, 2).indices[0, 1].item() if next_row < 0 or next_row >= n_rows or next_col < 0 or next_col >= n_cols else action
     generated_actions.append(index_to_action[action])
-    print(int(state/20), state%20, index_to_action[action])
+    print(int(state/n_rows), state%n_cols, index_to_action[action])
     print("Next Position:", next_row, next_col)
 
     next_row, next_col = calculate_next_state(state, action)
     next_state_idx = next_row * n_cols + next_col
     
-    done = True if (next_row == 19 and next_col == 19) else False  # Check if goal reached
+    done = True if (next_row == goal_field[0] and next_col == goal_field[1]) else False  # Check if goal reached
 
     state = next_state_idx
     step_count += 1
